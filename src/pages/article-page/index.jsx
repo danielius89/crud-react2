4895c84cadf9e2c36e5e0682e8e00c339c2adc1b
Danielius <danielius.zav@gmail.com>
2022-10-18 19:16:24 +0300

    feat: finished designing article page

--- src/pages/article-page/index.jsx
+++ src/pages/article-page/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import { useParams } from 'react-router-dom';
 import {
   Box, Alert, Divider, Typography,
 } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import ButtonBase from '@mui/material/ButtonBase';
+import Chip from '@mui/material/Chip';
+import MovieIcon from '@mui/icons-material/Movie';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import Stack from '@mui/material/Stack';
 import ArticleService from '../../services/news-service';
 
 const ArticlePage = () => {
@@ -20,6 +29,12 @@ const ArticlePage = () => {
       }
     })();
   }, [articleId]);
+  const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  });
 
   return (
     <Box sx={{ mt: 4, mx: 4 }}>
@@ -27,10 +42,68 @@ const ArticlePage = () => {
       {article && (
       <Box>
         <Box>
-          <Typography variant="h4" sx={{ mb: 1 }}>{article.title}</Typography>
-          <Typography variant="body1">{article.description}</Typography>
+          <Paper
+            sx={{
+              p: 2,
+              margin: 'auto',
+              maxWidth: 1200,
+              flexGrow: 1,
+              backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#1A2027' : '#fff'),
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item>
+                <ButtonBase sx={{ maxWidth: 400, maxHeight: 400 }}>
+                  <Img alt="complex" src={article.img} />
+                </ButtonBase>
+              </Grid>
+              <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                  <Grid item xs>
+                    <Typography variant="h4" sx={{ mb: 1 }}>{article.title}</Typography>
+                    <Stack direction="row" spacing={1} paddingBottom={2}>
+
+                      {article.categoryId === '3' ? (
+                        <Chip icon={<VideogameAssetIcon />} label="Games" />
+                      )
+                        : ('')}
+
+                      {article.categoryId === '2' ? (
+                        <Chip icon={<MovieIcon />} label="Movies" />
+                      )
+                        : ('')}
+
+                      {article.categoryId === '1' ? (
+                        <Chip icon={<LiveTvIcon />} label="TV series" />
+                      )
+                        : ('')}
+                    </Stack>
+                    <Divider sx={{ marginBottom: 2 }} />
+
+                    <Typography variant="body1">{article.description}</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Divider sx={{ marginBottom: 2 }} />
+                    <Box component="div" sx={{ display: 'inline', marginRight: '15px', fontWeight: '700' }}>{article.author}</Box>
+                    <Box
+                      component="div"
+                      sx={{
+                        display: 'inline',
+                        marginRight: '15px',
+                        fontWeight: '400',
+                        fontStyle: 'italic',
+                      }}
+                    >
+                      {article.date}
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+
         </Box>
-        <Divider />
+
       </Box>
       )}
     </Box>
