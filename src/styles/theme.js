be516299503697493d1e865daf78527ea0ea3976
Danielius <danielius.zav@gmail.com>
2022-10-22 16:38:14 +0300

    feat: changed font

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,21 @@
 import { createTheme } from '@mui/material';
 
 const lightTheme = createTheme({
+  typography: {
+    fontFamily: 'Maven Pro, sans-serif',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Maven Pro, sans-serif';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+        }
+      `,
+    },
+  },
   palette: {
     background: {
       default: '#fafafa',
